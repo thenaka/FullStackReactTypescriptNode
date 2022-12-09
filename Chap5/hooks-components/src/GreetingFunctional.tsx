d9a689b0519ec8e46d464d583989ee69f8f7435f
Ryan Muranaka <rmuranaka@hotmail.com>
2022-12-09 08:16:32 -0700

    Completed hooks-components section of Chapter 5

---
+++ Chap5/hooks-components/src/GreetingFunctional.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+interface GreetingProps {
+	enteredName: string;
+	message: string;
+	greetingDispatcher: React.Dispatch<{ type: string, payload: string }>;
+}
+export default function Greeting(props: GreetingProps) {
+	console.log("rendering Greeting")
+	const onChangeName = (e: React.ChangeEvent<HTMLInputElement>) => {
+		props.greetingDispatcher({ type: "enteredName", payload: e.target.value });
+		props.greetingDispatcher({ type: "message", payload: e.target.value });
+	}
+	return (<div>
+		<input value={props.enteredName} onChange={onChangeName} />
+		<div>
+			{props.message}
+		</div>
+	</div>);
+}

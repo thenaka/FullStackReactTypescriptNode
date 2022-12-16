98bea6243d1df8eefebd4439ee5ec42ed1b943d5
Ryan Muranaka <rjmuranaka@gmail.com>
2022-12-16 14:22:31 -0700

    Work in progress on Chapter 6

---
+++ Chap6/ejected-app/src/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from './logo.svg';
+import DisplayText from "./DisplayText";
+import './App.css';
+
+function App() {
+    const getUserFullname = async (username: string): Promise<string> => {
+        const usersResponse = await fetch('https://jsonplaceholder.typicode.com/users');
+        if (usersResponse.ok) {
+            const users = await usersResponse.json();
+            const userByName = users.find((usr: any) => {
+                return usr.username.toLowerCase() === username;
+            });
+            return userByName.name;
+        }
+        return "";
+    }
+    return (
+        <div className="App">
+            <DisplayText getUserFullname={getUserFullname} />
+        </div>
+    );
+}
+
+export default App;

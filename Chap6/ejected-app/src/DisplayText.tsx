0e3aca2296ac16b770b5c93688da13e7a9a9f950
Ryan Muranaka <rjmuranaka@gmail.com>
2022-12-23 10:12:11 -0700

    I completed Chapter 6

--- Chap6/ejected-app/src/DisplayText.tsx
+++ Chap6/ejected-app/src/DisplayText.tsx
@@ -1,33 +1,42 @@
-import React, { useState } from "react";
-const DisplayText = () => {
-	const [txt, setTxt] = useState("");
-	const [msg, setMsg] = useState("");
-	const onChangeText = (e: React.ChangeEvent<HTMLInputElement>) => {
-		setTxt(e.target.value);
-	}
-	const onClickShowMsg = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-		e.preventDefault();
-		setMsg(`Welcome to React testing, ${txt}`);
-	}
+import React, { useState, FC } from "react";
+import UserTodos from "./UserTodos";
 
-	return (
-		<form>
-			<div>
-				<label>Enter your name</label>
-			</div>
-			<div>
-				<input data-testid="user-input" value={txt} onChange={onChangeText} ></input>
-			</div>
-			<div>
-				<button data-testid="input-submit" onClick={onClickShowMsg}>Show Message</button>
-			</div>
-			<div>
-				<label data-testid="final-msg">{msg}</label>
-			</div>
-			<div>
-				this is just a test entry
-			</div>
-		</form>
-	)
+interface DisplayTextProps {
+    getUserFullname: (username: string) => Promise<string>;
 }
+const DisplayText: FC<DisplayTextProps> = ({ getUserFullname }) => {
+    const [txt, setTxt] = useState("");
+    const [msg, setMsg] = useState("");
+    const [todoConrol, setTodoControl] = useState<ReturnType<typeof UserTodos>>();
+
+    const onChangeText = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setTxt(e.target.value);
+    }
+
+    const onClickShowMsg = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        e.preventDefault();
+        setTodoControl(null);
+        setMsg(`Welcome to React testing, ${await getUserFullname(txt)}`);
+        setTodoControl(<UserTodos username={txt} />);
+    }
+
+    return (
+        <form>
+            <div>
+                <label>Enter your name</label>
+            </div>
+            <div>
+                <input data-testid="user-input" value={txt} onChange={onChangeText} />
+            </div>
+            <div>
+                <button data-testid="input-submit" onClick={onClickShowMsg}>Show Message</button>
+            </div>
+            <div>
+                <label data-testid="final-msg">{msg}</label>
+            </div>
+            {todoConrol}
+        </form>
+    )
+}
+
 export default DisplayText;
